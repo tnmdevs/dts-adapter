c99e71fef82c6ce887e5c8f5ec44f88567e52511
Saul Chelewani <saulchelewani@gmail.com>
2022-03-16 15:08:52 +0200

    filter snapi bundles

--- src/Responses/QueryBundlesResponse.php
+++ src/Responses/QueryBundlesResponse.php
@@ -4,8 +4,14 @@ namespace TNM\DTS\Responses;
 
 class QueryBundlesResponse extends DTSResponse implements IQueryBundleResponse
 {
+
     public function getBundles(): array
     {
         return $this->response->json('data');
+    }
+
+    public function getSnapiBundles(): array
+    {
+        return collect($this->getBundles())->filter(fn (array $bundle) => $bundle['type'] == 'SNAPI')->toArray();
     }
 }
